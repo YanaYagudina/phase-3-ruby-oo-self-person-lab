8cb333c2831fd956386fd75fc35abde33eccbe00
Yana <yana.videoeditor.gmail.com>
2022-11-02 20:46:55 -0600

    commit tree a1b51e56c3acc8a2a2ff1fc3a6d37f478bf49487

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,38 @@
-# your code goes here
+class Person
+   attr_reader :name, :hygiene, :happiness
+   attr_accessor :bank_account
+    
+    def initialize (name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        @happiness = if num > 10
+                         10
+                    elsif num < 0
+                         0
+                    else 
+                        num
+                    end
+    end
+    def hygiene=(num)
+   # with clamp
+         @hygiene = num.clamp(0, 10)
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid (amount)
+        self.bank_account +=amount
+        "all about the benjamins"
+    end
+end
